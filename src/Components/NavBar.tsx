4561bad73f2ca9c21e16e32e7139a9160cf1eeb4
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-04 15:19:31 +0300

    movies

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,9 +1,9 @@
 function NavBar() {
     return (
-        <div className="border-b-2 border-b-blue w-full mb-10 mt-8 ml-12  shadow-2xl shadow-pink ">
-            <h1 className="text-6xl font-bold p-1.5 m-2.5">
-                Qasem
-            </h1>
+        <div className="flex justify-around   border-b-2 border-b-blue w-full mb-10 mt-8 ml-12  shadow-2xl shadow-pink ">
+            <div className="p-2.5">Home</div>
+            <div className="p-2.5">Content</div>
+            <div className="p-2.5">About</div>
         </div>
     );
 }
--- src/Components/RenderData.tsx
+++ src/Components/RenderData.tsx
@@ -7,7 +7,7 @@ function RenderData({data}) {
                 data.map((item: { id: number | null | undefined; image: string; title: string | undefined; }) => {
                     return(
                         <div className="bg-cyan-300 p-4 shadow-2xl shadow-pink  " key={item.id}>
-                            <img className="scale-90 rotate-2 border-b-2  4xl  hover:hue-rotate-180  "  src={ImagPath+item.backdrop_path} alt={item.title} />
+                            <img className="scale-90 rotate-2 border-b-2  4xl shadow-pink hover:hue-rotate-180  "  src={ImagPath+item.backdrop_path} alt={item.title} />
                         <h5 className="flex justify-center">{item.title}</h5>
                         </div>
                     )
