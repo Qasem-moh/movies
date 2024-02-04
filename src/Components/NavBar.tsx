4e0d0af42a8e497269b47e0bd9df6fe310a2e0c9
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-04 18:21:58 +0300

    movies

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,6 +1,6 @@
 function NavBar() {
     return (
-        <div className="flex justify-around   border-b-2 border-b-blue w-full mb-10 mt-8 ml-12  shadow-2xl shadow-pink ">
+        <div className="flex justify-around border-b-2 border-b-blue w-full mb-10 mt-8 ml-12  shadow-2xl shadow-pink ">
             <div className="p-2.5">Home</div>
             <div className="p-2.5">Content</div>
             <div className="p-2.5">About</div>
